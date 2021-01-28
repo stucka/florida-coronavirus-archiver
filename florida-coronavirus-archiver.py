14581abd820c4fdf61751077eb40f2722cd3cc38
Mike Stucka <stucka@whitedoggies.com>
2021-01-28 14:50:20 -0500

    More insurance

--- florida-coronavirus-archiver.py
+++ florida-coronavirus-archiver.py
@@ -3,6 +3,7 @@ from time import sleep
 
 archiveprefix = "https://web.archive.org/save/"
 naptime = 1   # Seconds to sleep between requests
+maxrequestwait = 20    # How long to wait before calling it quits on a connection?
 
 with open("things-to-archive.txt", "r") as infile:
     thingstoarchive = infile.read().splitlines()
@@ -12,7 +13,7 @@ for thingtoarchive in thingstoarchive:
         thingtoarchive = thingtoarchive.strip().split("#")[0].split()[0].strip()    # Lose all comments
         if len(thingtoarchive) > 0:   # If not a blank line:
             print(f"Trying to archive {thingtoarchive}")
-            r = requests.get(archiveprefix + thingtoarchive, allow_redirects=False, timeout=10)
+            r = requests.get(archiveprefix + thingtoarchive, allow_redirects=False, timeout=maxrequestwait)
             sleep(naptime)
     except:
         print(f"Error fetching {thingtoarchive}")
