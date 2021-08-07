5d5fb0bff63516b5db18e950e4c3fab67a930c55
Mike Stucka <stucka@whitedoggies.com>
2021-08-07 08:54:29 -0400

    State ended almost every report

--- florida-coronavirus-archiver.py
+++ florida-coronavirus-archiver.py
@@ -10,11 +10,12 @@ with open("things-to-archive.txt", "r") as infile:
 
 for thingtoarchive in thingstoarchive:
     try:
-        thingtoarchive = thingtoarchive.strip().split("#")[0].split()[0].strip()    # Lose all comments
-        if len(thingtoarchive) > 0:   # If not a blank line:
-            print(f"Trying to archive {thingtoarchive}")
-            r = requests.get(archiveprefix + thingtoarchive, allow_redirects=False, timeout=maxrequestwait)
-            sleep(naptime)
+        if len(thingtoarchive) > 0 and thingtoarchive[0] != "#":
+            thingtoarchive = thingtoarchive.strip().split("#")[0].split()[0].strip()    # Lose all comments
+            if len(thingtoarchive) > 0:   # If not a blank line:
+                print(f"Trying to archive {thingtoarchive}")
+                r = requests.get(archiveprefix + thingtoarchive, allow_redirects=False, timeout=maxrequestwait)
+                sleep(naptime)
     except:
         print(f"Error fetching {thingtoarchive}")
         sleep(naptime)
