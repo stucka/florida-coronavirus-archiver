0dd4b84684113cf19f40ed5728a34283766b8d46
Chad <github@chad.org>
2022-01-06 09:19:54 -0500

    Tighten exception scope, emit exceptions, and simplify

--- florida-coronavirus-archiver.py
+++ florida-coronavirus-archiver.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+import re
 
 archiveprefix = "https://web.archive.org/save/"
 naptime = 1   # Seconds to sleep between requests
@@ -9,13 +10,13 @@ with open("things-to-archive.txt", "r") as infile:
     thingstoarchive = infile.read().splitlines()
 
 for thingtoarchive in thingstoarchive:
-    try:
-        if len(thingtoarchive) > 0 and thingtoarchive[0] != "#":
-            thingtoarchive = thingtoarchive.strip().split("#")[0].split()[0].strip()    # Lose all comments
-            if len(thingtoarchive) > 0:   # If not a blank line:
-                print(f"Trying to archive {thingtoarchive}")
-                r = requests.get(archiveprefix + thingtoarchive, allow_redirects=False, timeout=maxrequestwait)
-                sleep(naptime)
-    except:
-        print(f"Error fetching {thingtoarchive}")
+    thingtoarchive = re.sub(r"#.*", "", thingtoarchive).strip()
+    if thingtoarchive:
+        print(f"Trying to archive {thingtoarchive}")
+        try:
+            requests.get(archiveprefix + thingtoarchive, allow_redirects=False, timeout=maxrequestwait)
+            # This looks like a GET that we discard, and it is, but the point is that it has the
+            # side-effect of making The Wayback Machine store any updated copy. Demand drives storage.
+        except Exception as exc:
+            print(f"Error fetching {thingtoarchive} because {exc}")
         sleep(naptime)
